ce891ed3d60b342ce3d3ea5dfd7b6cf19f8e36d7
dayton brock <dayton.brock@gmail.com>
2019-05-19 12:30:42 -0500

    html draft, minimal styling, created onSubmit that adds newEmployee object to array

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
-console.log( 'js' );
-
 $( readyNow );
+
+// global variables
+let employees = [];
 
 function readyNow(){
     console.log( 'JQ' );
+    // // `handleSubmitClick();`
+    $( '#submitBtn' ).on( 'click', onSubmit ); 
+
+function onSubmit(){
+    console.log('submit employee');
+    // get user input
+    let firstNameIn = $('#firstNameIn').val();
+    let lastNameIn = $('#lastNameIn').val();
+    let IDIn = $('#IDIn').val();
+    let titleIn = $('#titleIn').val();
+    let annualSalaryIn = $('#annualSalaryIn').val();
+    // create employee object
+    let newEmployee = {
+        firstName: firstNameIn,
+        lastName: lastNameIn,
+        ID: IDIn,
+        title: titleIn,
+        annualSalary: annualSalaryIn
+    } // end obj
+    // push to array of employees
+    employees.push(newEmployee);
+    console.log(employees);
+    // function displayEmployeeList(){
+
+    // }
+        
+    }
 }
+
+
+
+
+
+
+
+
+    // push to array of employees
+    
+    // // display employee objects to `#employeeTable`
+    // loop over array, for each employee
+    // append employee attributes
+}
+
